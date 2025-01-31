c82396e3dcf190bd0ff07486ec34dd9e0539d4fe
Josef Spjut <josef.spjut@gmail.com>
2025-01-31 11:48:02 -0500

    use unofficial FPSci v25.01.01

--- setup.bat
+++ setup.bat
@@ -1,9 +1,11 @@
-set FPSCI_VERSION=v23.04.01
+set FPSCI_VERSION=v25.01.01
 set HIDESEEK_FOLDER_ID=1hryO2ewrrQhKmApf-CizbCejUztCxEv9
 
 @REM Get FPSci build
+@REM official releases should use this path https://github.com/NVlabs/FPSci/releases/download/%FPSCI_VERSION%/FPSci.%FPSCI_VERSION%.zip
+@REM unofficial releases should use this path https://github.com/jspjut/FPSci/releases/download/%FPSCI_VERSION%/FPSci.%FPSCI_VERSION%.zip
 echo Downloading FPSci
-curl -LO https://github.com/NVlabs/FPSci/releases/download/%FPSCI_VERSION%/FPSci.%FPSCI_VERSION%.zip
+curl -LO https://github.com/jspjut/FPSci/releases/download/%FPSCI_VERSION%/FPSci.%FPSCI_VERSION%.zip
 echo Extracting FPSci
 mkdir FPSci
 tar -xf FPSci.%FPSCI_VERSION%.zip -C FPSci\
